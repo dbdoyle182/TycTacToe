81819da31a44d01a3f55f70a21554b4deec595a1
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-11 21:04:33 -0400

    added variable

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Input determines the order of the game as well as the symbol for player
 player1 = input("Please pick a marker 'X' or 'O' :")
 
+position = int(input('Please enter a number: '))
+
 def turnOrder(playerchoice):
     if(playerchoice) == 'x' or(playerchoice) == 'X':
         print("Player one has chosen X, please go first")
