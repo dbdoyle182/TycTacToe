2bd8959aca3386b0abc5a47a8197664470f8fa8e
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-11 21:24:12 -0400

    board displays

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ player1 = input("Please pick a marker 'X' or 'O' :")
 position = int(input('Please enter a number: '))
 
 def display_board(board):
-    print(" \n * 100")
+    print("\n"*100)
 
-    print('  |  |')
+    print('___|___|___')
+    print(' {} | {} | {} '.format(board[7],board[8],board[9]))
+    print('___|___|___')
+    print(' {} | {} | {} '.format(board[4],board[5],board[6]))
+    print('___|___|___')
+    print(' {} | {} | {} '.format(board[1],board[2],board[3]))
+
+
+    
 
 
