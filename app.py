b980273b4d7bb50ba5fca51526a16a9af22c2cb1
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-11 21:34:12 -0400

    winning logic

--- app.py
+++ app.py
@@ -14,7 +14,16 @@ def display_board(board):
     print('___|___|___')
     print(' {} | {} | {} '.format(board[1],board[2],board[3]))
 
-
+def winner(board):
+    if board[1] == board[2] == board[3] or board[4] == board[5] == board[6] or board[7] == board[8] == board[9]:
+        print('win')
+    elif board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[3] == board[6] == board[9]:
+        print('win')
+    elif board[3] == board[5] == board[7] or board[1] == board[5] == board[9]:
+        print('win')
+    else:
+        print('no one won, you suck')
+    
     
 
 
