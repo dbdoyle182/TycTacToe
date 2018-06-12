638d1dafd4bf309411b667c53543c53732299754
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-11 21:11:08 -0400

    added print function

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from IPython.display import clear_output
-
 # Input determines the symbol for player
 player1 = input("Please pick a marker 'X' or 'O' :")
 
 #Determines turn order
 position = int(input('Please enter a number: '))
 
+def display_board(board):
+    print(" \n * 100")
+
+    print('  |  |')
 
 
