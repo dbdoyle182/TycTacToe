be8b31dac1bf005bc8aa5248bb05c9222e245cdf
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-11 21:07:16 -0400

    removed else if statement

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-# Input determines the order of the game as well as the symbol for player
+from IPython.display import clear_output
+
+# Input determines the symbol for player
 player1 = input("Please pick a marker 'X' or 'O' :")
 
+#Determines turn order
 position = int(input('Please enter a number: '))
 
-def turnOrder(playerchoice):
-    if(playerchoice) == 'x' or(playerchoice) == 'X':
-        print("Player one has chosen X, please go first")
-        player2 = "O"
-    elif(playerchoice) =='o' or(playerchoice) == 'O':
-        print("Player one has chosen O, please go second")
-        player2 = "X"
 
-turnOrder(player1)
-
-print(player1)
-print(player2)
 
